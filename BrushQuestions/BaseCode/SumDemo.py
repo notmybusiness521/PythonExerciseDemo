7a5096555db5e00cd283762b1e33c2494b14dece
wjp <774627949@qq.com>
2020-06-06 12:29:05 +0800

    求和小Demo

---
+++ BrushQuestions/BaseCode/SumDemo.py
@@ -0,0 +1,14 @@
+# --*--coding=utf8--*--
+'''
+@Author:
+@Time:
+@Describe:
+'''
+
+import random
+
+if __name__ == '__main__':
+    sum = 0
+    for j in range(1, 101):
+        sum += j
+    print(sum)
